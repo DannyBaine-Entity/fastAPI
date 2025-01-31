2119984bb4dc3510e457d68c4511da7ad063c6cb
DannyBaine-Entity <apololadanieltolu@gmail.com>
2025-01-31 21:17:41 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
+import pytz
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all domains (or specify specific ones)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+@app.get("/")
+def get_info():
+    return {
+        "email": "apololadanieltolu@gmail.com",
+        "current_datetime": datetime.now(pytz.utc).isoformat(),
+        "github_url": "https://github.com/DannyBaine-Entity/fastAPI"
+    }
